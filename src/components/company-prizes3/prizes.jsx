47c62448e26e677f7c7eee86d719ea1966132ff4
Parth Saini <parthsaini13@gmail.com>
2023-12-14 00:38:01 +0530

    add some feature

--- src/components/company-prizes3/prizes.jsx
+++ src/components/company-prizes3/prizes.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
 import './styles.css';
 import { company } from './data';
-
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 function YourComponent() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
-       <div className="flex items-center justify-center bg-transparent relative" style={{ height: '10rem' }}>
+       <div data-aos="fade-up"  data-aos-duration="2000" className=" flex items-center justify-center bg-transparent relative" style={{ height: '10rem' }}>
         <div className="header absolute top-0 text-left" style={{ width: '80%' }}>
           <h2 className="header1">TIER-3</h2>
           <h2 className="header2">THE TRACK PRIZES</h2>
         </div>
       </div>
-      <div className="container">
+      <div  data-aos="fade-right"  data-aos-duration="2000" className="container">
         {company.map((company, index) => (
           <div className="card" style={{ '--clr': `#${index + 1}E${index + 2}7F` }} key={index}>
             <div className="img-box">
