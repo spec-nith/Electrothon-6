6ff7801424be1d941c02c903856fb7b1b85f87fd
Parth Saini <parthsaini13@gmail.com>
2023-12-16 01:33:18 +0530

    fix prize page

---
+++ src/components/prizes-nav/button/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+const Button = () => {
+  return (
+    
+      <a href="#" className=" txt let button-wiggle button-pulse">
+        REGISTER TO PARTICIPATE →
+      </a>
+   
+  );
+};
+
+export default Button;
