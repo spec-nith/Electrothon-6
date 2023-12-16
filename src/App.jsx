155e5a809915e1f604b45cd168bb21686dcc2451
Parth Saini <parthsaini13@gmail.com>
2023-12-16 23:36:51 +0530

    added in nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Home from './Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sponser from './components/Sponser/Sponser';
+import Pirzenav from './components/prizes-nav/prizes-main/prize';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sponser" element={<Sponser/>} />
+        <Route path="/Pirzenav" element={<Pirzenav/>} />
       </Routes>
     </Router>
   );
