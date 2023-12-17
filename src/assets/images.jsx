af0cfda5e9eacb7abb17a6b1e6252c02d37afab8
Akshanshkaushal <akshanshkaushal9@gmail.com>
2023-12-18 00:43:27 +0530

    fixed_changes

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,57 +1,80 @@
-
-import bg from "../assets/images/background.png"
-import bg2 from "../assets/images/bg2.jpg"
-import bg3 from "../assets/images/bg3.jpg"
-import headerimg from "../assets/images/headerimg.png"
-import chamkadar from "../assets/images/chamkadar.png"
-import man_demo from "../assets/images/man_demo.png"
-import back_demo from "../assets/images/demo_back.png" 
-import phy_demo from "../assets/images/phy_demo.png"
-import phy_l_demo from "../assets/images/phy_l_demo.png"
-import l_demo from "../assets/images/demo_l.png"
-import why_sponser from "../assets/images/why_sponser.png"
-import PrizeIcon1 from "../assets/images/PrizeIcon1.png"
-import PrizeIcon2 from "../assets/images/PrizeIcon2.png"
-import PrizeIcon3 from "../assets/images/PrizeIcon3.png"
-import prizee from "../assets/images/prize.svg"
-import whysponserfire from "../assets/images/whysponsorfire.png"
-import whysponser1 from "../assets/images/whysponsor1.png"
-import whysponser2 from "../assets/images/whysponsor2.png"
-import whysponser3 from "../assets/images/whysponsor3.png"
+ 
 import aditya from "../assets/images/aditya.jpg.png"
 import devang from "../assets/images/Devang.jpeg.png"
 import gaurav from "../assets/images/gaurav.jpg.png"
 import insha from "../assets/images/insha.jpg.png"
 import kundan from "../assets/images/kundan.jpeg.png"
 import rodrigo from "../assets/images/rodrigo.jpeg.png"
+import bg from "../assets/images/background.png";
+import bg2 from "../assets/images/bg2.jpg";
+import bg3 from "../assets/images/bg3.jpg";
+import headerimg from "../assets/images/headerimg.png";
+import chamkadar from "../assets/images/chamkadar.png";
+import man_demo from "../assets/images/man_demo.png";
+import back_demo from "../assets/images/demo_back.png";
+import phy_demo from "../assets/images/phy_demo.png";
+import phy_l_demo from "../assets/images/phy_l_demo.png";
+import l_demo from "../assets/images/demo_l.png";
+import why_sponser from "../assets/images/why_sponser.png";
 import why_sponserbg from "../assets/images/why_sponserbg.png";
+import PrizeIcon1 from "../assets/images/PrizeIcon1.png";
+import PrizeIcon2 from "../assets/images/PrizeIcon2.png";
+import PrizeIcon3 from "../assets/images/PrizeIcon3.png";
+import prizee from "../assets/images/prize.svg";
+import support1 from "../assets/images/support1.png";
+import support2 from "../assets/images/support2.svg";
+
+import support4 from "../assets/images/support4.svg";
+import support5 from "../assets/images/support5.svg";
+
+import support7 from "../assets/images/support7.svg";
+import support8 from "../assets/images/support8.svg";
+import support9 from "../assets/images/support9.svg";
+import support10 from "../assets/images/support10.svg";
+import support11 from "../assets/images/support11.svg";
+import support12 from "../assets/images/support12.svg";
+import support13 from "../assets/images/support13.png";
+import support14 from "../assets/images/support14.png";
+import support15 from "../assets/images/support15.png";
+import support16 from "../assets/images/support16.png";
+import whysponser2 from "../assets/images/whysponser2.png"
 
 export {
-    bg
-    ,bg2,
-    bg3,
-    headerimg,
-    chamkadar,
-    man_demo,
-        back_demo,
-        phy_demo,
-        phy_l_demo,
-        l_demo,
-        why_sponser,
-        PrizeIcon1,
-        PrizeIcon2,
-        PrizeIcon3,
-    prizee,
-    whysponserfire,
-    whysponser1,
-    whysponser2,
-    whysponser3,
-    aditya,
-    insha,
-    gaurav,
-    rodrigo,
-    devang,
-    kundan,
-  why_sponserbg
+  bg,
+  bg2,
+  bg3,
+  headerimg,
+  chamkadar,
+  man_demo,
+  back_demo,
+  phy_demo,
+  phy_l_demo,
+  l_demo,
+  why_sponser,
+  PrizeIcon1,
+  PrizeIcon2,
+  PrizeIcon3,
+  prizee,
+  support1,
+  support2,
+  support4,
+  support5,
+  support7,
+  support8,
+  support9,
+  support10,
+  support11,
+  support12,
+  support13,
+  support14,
+  support15,
+  support16,
+  why_sponserbg,
+  whysponser2,
+  aditya,
+  insha,
+  gaurav,
+  rodrigo,
+  devang,
+  kundan,
 };
-
