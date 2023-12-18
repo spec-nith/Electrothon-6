d4b77b2059aa111a143f748a93cca2136ec8c8aa
Akshansh Kaushal <113452367+Akshanshkaushal@users.noreply.github.com>
2023-12-18 05:37:13 +0530

    Update hero.jsx

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,45 +1,56 @@
-import React from 'react';
-import Timer from '../CountDown/countDown';
-import MagicCanvas from '../../canvas/Magic_stand';
- 
-import CrookCanvas from '../../canvas/Croo_k';
- 
+ import React from "react";
+import "./hero.css";
+import Timer from "../CountDown/countDown";
+import MagicCanvas from "../../canvas/Magic_stand";
+
+import CrookCanvas from "../../canvas/Croo_k";
 
 const Hero = () => {
   return (
     <>
-   
-    <div className="flex w-full h-full z-[20] items-center justify-center min-h-screen bg-transparent relative">
- {/* left */}
-      <div className="hidden sm:block absolute left-0 top-1/2 transform -translate-y-1/2 w-1/4 h-full object-cover opacity-50">
-       <CrookCanvas/>
-</div>
+      <div className="flex w-full h-full z-[20] items-center justify-center min-h-screen bg-transparent relative">
+        {/* left */}
+        <div className="hidden sm:block absolute left-0 top-1/2 transform -translate-y-1/2 w-1/4 h-full object-cover opacity-50">
+          <CrookCanvas />
+        </div>
 
-      {/* Content Container */}
-      <div className=" absolute top-0 z-10 text-center  p-8  md:p-8 ">
-        <h1 className="lg:text-7xl
-        sm:text-5xl text-4xl  font-bold mb-4  text-white"><h5 className='lg:text-3xl
-        sm:text-xl text-base font-semibold my-3 tracking-wide '>A CROSS COUNTRY HACKATHON</h5>
-          ELECTROTHON <br/> <small className='lg:text-7xl
-        sm:text-5xl text-4xl font-semibold' >
-          RELEVELED
-          </small>
-        </h1>
-        <p className="text-lg text-gray-400 ">
-        Join electrothon relevel organised by spec NITH to create one of your <br/>
-coolest project and netwerk with craxy minded people
-        </p>
-        <div>
-            <Timer/>
+        {/* Content Container */}
+        <div className=" same absolute top-0 z-10 text-center  p-8  md:p-8 ">
+          <h1
+            className="lg:text-7xl
+        sm:text-5xl text-4xl  font-bold mb-4  text-white"
+          >
+            <h5
+              className="lg:text-3xl
+        sm:text-xl text-base font-semibold my-3 tracking-wide "
+            >
+              A CROSS COUNTRY HACKATHON
+            </h5>
+            <span className="headesign">
+              ELECTROTHON <br />{" "}
+            </span>
+            <small
+              className=" head2design lg:text-7xl
+        sm:text-5xl text-4xl font-semibold"
+            >
+              RELEVELED
+            </small>
+          </h1>
+          <p className="text-lg text-gray-400 ">
+            Join electrothon relevel organised by spec NITH to create one of
+            your <br />
+            coolest project and netwerk with craxy minded people
+          </p>
+          <div>
+            <Timer />
+          </div>
+        </div>
+
+        {/* right */}
+        <div className="hidden sm:block absolute top-0 right-0 h-full w-1/4">
+          <MagicCanvas />
         </div>
       </div>
-
-  {/* right */}
-<div className='hidden sm:block absolute top-0 right-0 h-full w-1/4'>
-   <MagicCanvas/>
-</div>
-    </div>
-
     </>
   );
 };
