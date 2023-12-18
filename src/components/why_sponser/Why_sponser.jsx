6b7c46fac51dba3eef9927e1edf38971f1b42313
Akshansh Kaushal <113452367+Akshanshkaushal@users.noreply.github.com>
2023-12-18 05:45:09 +0530

    Update Why_sponser.jsx

--- src/components/why_sponser/Why_sponser.jsx
+++ src/components/why_sponser/Why_sponser.jsx
@@ -1,23 +1,23 @@
- import React from 'react'
+ import React from "react";
 import { motion } from "framer-motion";
 import { TitleText } from "../Tracks_new/CustomTexts";
 import { staggerContainer } from "../Tracks_new/motion";
 import styles from "../Tracks_new/style";
-import { why_sponser, why_sponserbg} from '../../assets/images';
-import Design_copmponent from './opacity';
-import NumberCounter from './Number_Counter';
+import { why_sponser, why_sponserbg } from "../../assets/images";
+import Design_copmponent from "./opacity";
+import NumberCounter from "./Number_Counter";
+import "./why.css";
 
 export default function Why_sponser() {
-
   const divStyle = {
     backgroundImage: `url(${why_sponserbg})`,
-    backgroundSize: 'cover', 
-    backgroundRepeat: 'no-repeat',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
   };
 
   return (
-    <div className='text-white mt-24 min-h-screen' style={divStyle}>
-          <motion.div
+    <div className="text-white mt-24 min-h-screen" style={divStyle}>
+      <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
@@ -28,33 +28,28 @@ export default function Why_sponser() {
           title={
             <>
               <div className=" eur text-2xl lg:text-7xl font-extrabold text-white my-4 lg:my-6 font-[Oswald]">
-              WHY SPONSOR OUR EUREKA?
-               </div>
+                WHY SPONSOR OUR EUREKA?
+              </div>
             </>
           }
           textStyles="text-center"
         />
       </motion.div>
 
-<div className='flex flex-col sm:flex-row justify-center items-center mt-10'>
-      <div className='w-full flex flex-col justify-center items-center'>
-        <div className='sm:ml-16 mt-8 mb-2 font-bold text-3xl'>ELECTROTHON_ Hackathons : Where elite <br/>
-        students get to build great projects
-        </div>
-        <Design_copmponent />
-         <div className='flex flex-row'>
-           <NumberCounter number={500} data={"developers"}/>
+      <div className="flex flex-col sm:flex-row justify-center items-center mt-10">
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="sm:ml-16 mt-8">content</div>
+          <Design_copmponent />
+          <div className="flex flex-row">
+              <NumberCounter number={500} data={"developers"}/>
            <NumberCounter number={100} data={"Projects launched"}/>
            <NumberCounter number={50} data={"From the sponspors"}/>
+          </div>
+        </div>
+        <div className="flex flex-row mt-12 items-center w-1/2 justify-center">
+          <img src={why_sponser} alt="why_sponser" />
         </div>
       </div>
-      <div className='flex flex-row mt-12 items-center w-1/2 justify-center'>
- <img 
- src={why_sponser}
-alt="why_sponser"
-/>
-      </div>
-      </div>
     </div>
-  )
+  );
 }
