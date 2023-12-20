30a6807756a7cc5af83d99ef747345d1666b92ba
Parth Saini <parthsaini13@gmail.com>
2023-12-21 02:15:13 +0530

    time frame of carousel fixed

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './style.css';
 
 const Slider = () => {
+  const items = Array.from({ length: 50 }, (_, index) => index + 1)
   return (
     <div className="slider">
       <div className="slide-track">
-        {[1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7].map((index) => (
+      {items.map((index) => (
           <div className="slide" key={index}>
           . ELECTROTHON 6.0
           </div>
