3bdb5cd8b6c6aa12dc80e68696c6c3e28a928d2b
Akshansh Kaushal <113452367+Akshanshkaushal@users.noreply.github.com>
2023-12-20 16:00:23 +0530

    Update countDown.jsx

--- src/components/CountDown/countDown.jsx
+++ src/components/CountDown/countDown.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
+ import React, { useEffect, useRef, useState } from "react";
 
 const Timer = () => {
-  // const [timeUp,setTimeUp]=useState(false);
   const [timerDays, setTimerDays] = useState("--");
   const [timerHours, setTimerHours] = useState("--");
   const [timerMinutes, setTimerMinutes] = useState("--");
@@ -11,18 +10,15 @@ const Timer = () => {
 
   const startTimer = () => {
     const countDownDate = new Date("2024-02-16T18:00:00.000+05:30").getTime();
-    interval = setInterval(() => {
+    interval.current = setInterval(() => {
       const now = new Date().getTime();
       const distance = countDownDate - now;
       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance / 1000) % 60);
       if (distance < 0) {
         clearInterval(interval.current);
-        // setTimeUp(true);
       } else {
         setTimerDays(days);
         setTimerHours(hours);
@@ -31,76 +27,79 @@ const Timer = () => {
       }
     }, 1000);
   };
+
   useEffect(() => {
-    let intervalref = null;
+    let intervalRef = null;
     startTimer();
     if (interval.current) {
-      intervalref = interval.current;
+      intervalRef = interval.current;
     }
     return () => {
-      clearInterval(intervalref);
+      clearInterval(intervalRef);
     };
-  });
+  }, []);
+
   return (
-    <section id="timer" className="relative overflow-hidden lg:mt-8 mt-16">
+    <section id="timer" className="relative lg:mt-8 mt-16">
       <div className="container mx-auto">
         <div className="flex flex-col items-center w-full text-white">
-          <div className="flex md:items-stretch md:p-10">
-            <div className="flex flex-col items-center counter-cell w-18r md:w-32">
-              <span className="text-xl md:text-6xl lg:text-7xl p-3 md:p-5 subhead-theme">
+          <div className="flex md:flex-col">
+            <div className="counter-cell mb-4 md:mb-0">
+              <span className="text-2xl md:text-6xl lg:text-7xl subhead-theme">
                 {timerDays}
               </span>
               <div
                 className=" w-full text-center p-1.5 md:p-3 md:text-xl rounded-b-lg"
                 style={{ backgroundColor: "#1b141c" }}
               >
-                <span className="">Days</span>
+                <span className="text-sm">Days</span>
               </div>
             </div>
-            <span className="text-xl md:text-8xl px-1 md:px-2 pt-4">:</span>
-            <div className="flex flex-col items-center counter-cell w-18r md:w-32">
-              <span className="text-xl md:text-6xl lg:text-7xl p-3 md:p-5 subhead-theme">
+            <div className="divider md:hidden">:</div>
+            <span className="hidden md:block  md:text-8xl md:px-2 pt-4">:</span>
+            <div className="counter-cell mb-4 md:mb-0">
+              <span className="text-2xl md:text-6xl lg:text-7xl subhead-theme">
                 {("0" + timerHours).slice(-2)}
               </span>
               <div
                 className=" w-full text-center p-1.5 md:p-3 md:text-xl rounded-b-lg"
                 style={{ backgroundColor: "#1b141c" }}
               >
-                <span className="">Hours</span>
+                <span className="text-sm">Hours</span>
               </div>
             </div>
-            <span className="text-xl md:text-8xl px-1 md:px-2 pt-4">:</span>
-            <div className="flex flex-col items-center counter-cell w-18r md:w-32">
-              <span className="text-xl md:text-6xl lg:text-7xl p-3 md:p-5 subhead-theme">
+            <div className="divider md:hidden">:</div>
+            <span className="hidden md:block  md:text-8xl md:px-2 pt-4">:</span>
+            <div className="counter-cell mb-4 md:mb-0"
+            >
+              <span className="text-2xl md:text-6xl lg:text-7xl subhead-theme">
                 {("0" + timerMinutes).slice(-2)}
               </span>
               <div
                 className=" w-full text-center p-1.5 md:p-3 md:text-xl rounded-b-lg"
                 style={{ backgroundColor: "#1b141c" }}
               >
-                <span className="">Minutes</span>
+                <span className="text-sm">Minutes</span>
               </div>
             </div>
-            <span className="text-xl md:text-8xl px-1 md:px-2 pt-4">:</span>
-            <div className="flex flex-col items-center counter-cell w-18r md:w-32">
-              <span className="text-xl md:text-6xl lg:text-7xl p-3 md:p-5 subhead-theme">
+            <div className="divider md:hidden">:</div>
+            <span className="hidden md:block  md:text-8xl md:px-2 pt-4">:</span>
+            <div className="counter-cell">
+              <span className="text-2xl md:text-6xl lg:text-7xl subhead-theme">
                 {("0" + timerSeconds).slice(-2)}
               </span>
               <div
                 className=" w-full text-center p-1.5 md:p-3 md:text-xl rounded-b-lg"
                 style={{ backgroundColor: "#1b141c" }}
               >
-                <span className="">Seconds</span>
+                <span className="text-sm">Seconds</span>
               </div>
             </div>
           </div>
-          {/* } */}
         </div>
-        {/* <div className="w-full text-center text-xl content-theme">
-                <span>The tech realm is going to reign supreme on 18th February 2022 !</span>
-                </div> */}
       </div>
     </section>
   );
 };
+
 export default Timer;
