be205969048bb604895bc52b717d6bbdae9270f7
Hammad Hashmi <121767424+Hammadh7@users.noreply.github.com>
2024-01-04 10:09:20 +0530

    Update sponser.jsx

--- src/components/sponsernav/oursponser/sponser.jsx
+++ src/components/sponsernav/oursponser/sponser.jsx
@@ -119,9 +119,7 @@ const ImageGallery = () => {
       </ul>
     
     </div>
-     <div className="flex justify-center sm:mt-20 text-center text-sm mx-4 sm:text-xl pb-7 text-gray-100">
-    Ready to cast a spell of success. Mail us at:<br/>
-<a href='mailto:spec@nith.ac.in' className='text-blue-500'> &nbsp; spec@nith.ac.in</a>
+    
 
      </div>
     
