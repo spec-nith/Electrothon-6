52144f46859ff029d72397bb7338b6a7018866f3
Hammad Hashmi <121767424+Hammadh7@users.noreply.github.com>
2024-01-04 10:08:01 +0530

    Update header.jsx

--- src/components/sponsernav/header/header.jsx
+++ src/components/sponsernav/header/header.jsx
@@ -3,7 +3,10 @@ import './style.css';
 const YourComponent = () => {
   return (
     <div className="lel flex items-center justify-center  bg-transparent relative">
-      <div className="header absolute top-0 text-center" style={{ width: '80%' }}>
+        <div className="flex justify-center sm:mt-20 text-center text-sm mx-4 sm:text-xl pb-7 text-gray-100">
+    Ready to cast a spell of success. Mail us at:<br/>
+<a href='mailto:spec@nith.ac.in' className='text-blue-500'> &nbsp; spec@nith.ac.in</a>
+          <div className="header absolute top-0 text-center" style={{ width: '80%' }}>
         <h1 className="heads">OUR SPONSORS</h1>
       </div>
     </div>
