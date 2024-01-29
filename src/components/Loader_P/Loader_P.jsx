e8dbd01fac565f67ae4b5ceb49c4605accd02f87
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-01-29 19:35:06 +0530

    loader

--- src/components/Loader_P/Loader_P.jsx
+++ src/components/Loader_P/Loader_P.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Loader.css";
 
 const MagicalLoader = () => {
-  return <div className="loader"></div>;
+  return <div className="loader flex flex-col justify-center items-center"></div>;
 };
 
 export default MagicalLoader;
