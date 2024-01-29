b8e5d91b54f86b645749b1afa26bf24e3a6c1454
Parth Saini <parthsaini13@gmail.com>
2024-01-29 23:24:34 +0530

    fixed prizes for mobile

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,4 @@
- // Home.js
+// Home.js
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Hero from "../components/Hero/hero";
@@ -7,7 +7,7 @@ import Faqs from "../components/Faq/faq";
 import Footer from "../components/Footer/footer";
 import Challenges from "../components/Challenges/Challenges";
 import SpeakersNew from "../components/Speaker/speaker";
- 
+
 import Prize from "../components/Prizes/Prizes";
 import Timeline from "../components/Timeline/Timeline";
 import Organising_team from "../components/Organising_Team/Organising_team";
@@ -15,25 +15,24 @@ import Theme from "../components/Themes/Theme";
 import Carousel from "../components/carousel/carousel";
 import Why_sponser from "../components/why_sponser/Why_sponser";
 import StarsCanvas from "../canvas/Star";
- 
+
 import Support from "../components/Support/support";
- 
+
 import MagicalLoader from "../components/Loader_P/Loader_P";
 import Prizes_n from "../components/prizesnav/prizesmain/prize";
 import Sponsers from "../components/sponsernav/main/main";
- 
+
 import Organizers from "../components/Organizers_slider/Organizers";
 
- import { magicbg,magicbg2 } from "../assets/images";
+import { magicbg, magicbg2 } from "../assets/images";
 import BgStatic from "../components/Background/bgstatic";
 import Explore from "../components/Tracks_new/Track";
 
 function Home() {
-
   const [Loading, setLoading] = useState(false);
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   useEffect(() => {
     const onPageLoad = () => {
       setLoading(true);
@@ -49,13 +48,12 @@ function Home() {
   return (
     <>
       {!Loading ? (
-         <MagicalLoader/>
+        <MagicalLoader />
       ) : (
-      <div>
-
-          <div className="relative"> 
-          <BgStatic/>  
-          {/* <div className="fixed top-0 right-0 ">
+        <div>
+          <div className="relative">
+            <BgStatic />
+            {/* <div className="fixed top-0 right-0 ">
           <a
         id="mlh-trust-badge"
         className="mlh"
@@ -71,38 +69,39 @@ function Home() {
         />
       </a> 
       </div>      */}
-            <div className=""><StarsCanvas /></div>
+            <div className="">
+              <StarsCanvas />
+            </div>
             <Hero />
           </div>
           <div
-  //      className="bg-fixed bg-no-repeat "
-  //      style={{
-  //   backgroundImage: `url(${magicbg})`,
-  //  }}
- >
+          //      className="bg-fixed bg-no-repeat "
+          //      style={{
+          //   backgroundImage: `url(${magicbg})`,
+          //  }}
+          >
+            <AboutSection />
 
-          <AboutSection />
-      
-          <Timeline />
-          <Why_sponser />
-          <Carousel />
-          <Sponsers/>
-          {/* <div className="hidden 2xl:flex">
+            <Timeline />
+            <Why_sponser />
+            <Carousel />
+            <Sponsers />
+            {/* <div className="hidden 2xl:flex">
           <Theme />
           </div> */}
-          <div className=" block">
-          <Explore/>
+            <div className=" block">
+              <Explore />
+            </div>
+            <Prizes_n />
+            <Challenges />
+            <SpeakersNew />
+            <Organising_team />
+            {/* <Organizers /> */}
+            {/* <Support /> */}
+            <Faqs />
+            <Footer />
           </div>
-          <Prizes_n/>
-          <Challenges />
-          <SpeakersNew />
-          <Organising_team />
-          {/* <Organizers /> */}
-          {/* <Support /> */}
-          <Faqs />
-          <Footer />
         </div>
-      </div>
       )}
     </>
   );
