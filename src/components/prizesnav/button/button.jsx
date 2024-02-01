f8bd727b9818ae3c0e29c2c17030847db0535d9f
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-02-02 03:42:27 +0530

    button

--- src/components/prizesnav/button/button.jsx
+++ src/components/prizesnav/button/button.jsx
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <div className="text-white p-1 flex justify-center items-center">
       <div
-        className="apply-button w-[15rem] h-12 md:w-[20rem] "
+        className="apply-button w-[13rem] h-12 md:w-[20rem] "
         data-hackathon-slug="electrothon6"
         data-button-theme="light"
          
