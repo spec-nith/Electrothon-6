59cf3d47a6488c5814859f340d34e22f5ac90ef9
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-02-02 03:20:06 +0530

    responsive

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 import Hero from "../components/Hero/hero";
 import AboutSection from "../components/About/about";
 import Faqs from "../components/Faq/faq";
-import Footer from "../components/Footer/footer";
+ 
 import Challenges from "../components/Challenges/Challenges";
 import SpeakersNew from "../components/Speaker/speaker";
 
@@ -78,7 +78,7 @@ function Home() {
             <Organising_team />
           
             <Faqs />
-         
+            
             </div>
           </div>
         </div>
