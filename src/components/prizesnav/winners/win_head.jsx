3e60c5946ff1c36ae88d992cd97b2c90366ab1d8
Parth Saini <parthsaini13@gmail.com>
2024-02-07 19:25:21 +0530

    font fixed

--- src/components/prizesnav/winners/win_head.jsx
+++ src/components/prizesnav/winners/win_head.jsx
@@ -1,58 +1,62 @@
- import React, { useState, useEffect } from 'react';
-import './style.css';
-import { sport } from '../../../assets/images';
-import { gold,silver,bronze } from '../../../assets/images';
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { sport } from "../../../assets/images";
+import { gold, silver, bronze } from "../../../assets/images";
 
-
-function TopPrize({ position, prizeAmount,image }) {
- 
+function TopPrize({ position, prizeAmount, image }) {
   return (
-    
     <div className="flex flex-row items-center justify-center gap-2sm:gap-4 md:gap-6 lg:gap-12 w-full my-4 text-white">
       <div className="flex items-center justify-center w-[5rem] h-[5rem] md:w-24 md:h-24 text-2xl font-semibold text-white rounded-full">
-            <img src= {image === 1 ? gold : (image === 2 ? silver : bronze)} />      
+        <img src={image === 1 ? gold : image === 2 ? silver : bronze} />
       </div>
-      
+
       <div className="flex flex-col items-start w-[14rem] md:w-[12rem] ml-4 text-center md:ml-16 cursor-pointer">
         <span className="text-xl font-bold md:text-2xl ">
-          &nbsp;{position === 1 ? 'First' : (position === 2 ? 'Second' : 'Third')} Prize
+          &nbsp;{position === 1 ? "First" : position === 2 ? "Second" : "Third"}{" "}
+          Prize
         </span>
       </div>
       <div className="flex flex-col items-start ml-4 text-center md:ml-16 cursor-pointer">
-        <span className={`text-xl font-bold my-2 ${position === 1 ? 'text-yellow-400' : (position === 2 ? 'text-red-500' : 'text-blue-500')}`}>
-        {prizeAmount}  
+        <span
+          className={`text-xl font-bold my-2 ${
+            position === 1
+              ? "text-yellow-400"
+              : position === 2
+              ? "text-red-500"
+              : "text-blue-500"
+          }`}
+        >
+          {prizeAmount}
         </span>
       </div>
-    
     </div>
   );
 }
 
 function Winner() {
   useEffect(() => {
-    const defineText = document.querySelector('.define');
+    const defineText = document.querySelector(".define");
 
     const handleScroll = () => {
       const distanceFromTop = defineText.getBoundingClientRect().top;
       const screenHeight = window.innerHeight;
 
       if (distanceFromTop < screenHeight) {
-        defineText.classList.add('animated');
+        defineText.classList.add("animated");
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <div className="lel flex items-center justify-center  bg-transparent relative" >
-   <div className="header absolute  text-center" >
+    <div className="lel flex items-center justify-center  bg-transparent relative">
+      <div className="header absolute  text-center">
         <h1 className="heads">OUR PRIZES</h1>
- 
       </div>
     </div>
   );
@@ -78,7 +82,6 @@ function Prizes() {
       {showModal ? (
         <>
           <div className="fixed inset-0 z-50  flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-            
             <div className="relative w-auto max-w-3xl mx-auto my-6">
               {/** Content */}
               <div className="relative flex flex-col w-full bg-[#4a4a61] border-0 rounded-lg shadow-lg outline-none focus:outline-none text-white">
@@ -94,33 +97,35 @@ function Prizes() {
                     </span>
                   </button>
                 </div>
-                
+
                 {/** Body */}
                 <div className="relative flex-auto p-6">
-                  
                   <p className="my-4 text-lg leading-relaxed text-slate-500">
                     {modalData}
                   </p>
                 </div>
               </div>
-              
             </div>
           </div>
           <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
         </>
       ) : null}
       <Winner />
-      <div id="left-prize" className=" flex flex-col justify-center items-center w-full p-8  mt-16 md:mt-0">
- 
+      <div
+        id="left-prize"
+        className=" flex flex-col justify-center items-center w-full p-8  mt-16 md:mt-0"
+      >
         <TopPrize position={1} prizeAmount={"To be announced Soon"} image={1} />
-        <TopPrize position={2} prizeAmount={"To be announced Soon"} image={2}  />
+        <TopPrize position={2} prizeAmount={"To be announced Soon"} image={2} />
         <TopPrize position={3} prizeAmount={"To be announced Soon"} image={3} />
-        
-        <div id="left-prize" className="flex flex-col  p-8  md:w-2/5   mt-16 md:mt-0">
+
+        <div
+          id="left-prize"
+          className="flex flex-col  p-8  md:w-2/5   mt-16 md:mt-0"
+        >
           <img src={sport} alt="" />
         </div>
       </div>
-    
     </>
   );
 }
