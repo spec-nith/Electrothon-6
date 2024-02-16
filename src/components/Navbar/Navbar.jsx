80aa02e955d80f1effdb679fc68b55c1e269db6a
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-02-17 00:53:21 +0530

    mlh added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <picture style={{ width: "20%" }}>
         <img
-          className=" flex w-16 md:w-32 md:mr-4 absolute left-2 top-2"
+          className=" flex w-16 md:w-32 md:mr-4 absolute left-2 top-6 sm:top-2"
           src={speclogo}
           alt="spec_logo"
         />
