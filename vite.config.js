0bf456aa263c100ceff056f195d536a2ea6baea9
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-03-04 19:24:23 +0530

    cname

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: { chunkSizeWarningLimit: 25000, },
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, "index.html"),
-      404: resolve(__dirname, "public/404.html"),
-    },
-  },
+  base: '/'
+
+  
 
 })
