d60298c49fc35bfab7ca278d16a7b897ae8a2128
Prajualit Tickoo <prajualitt52626@gmail.com>
2024-09-08 17:36:15 +0530

    graph commit
    temporary

--- src/components/why_sponser/graph.jsx
+++ src/components/why_sponser/graph.jsx
@@ -3,6 +3,12 @@ import "./graph.css";
 import React, { useEffect, useState } from "react";
 
 const ChartSection = () => {
+  const dataElectrothon6 = [
+    { value: 1500, label: "N&J" },
+    { value: 63, label: "Fraser" },
+    { value: 20, label: "Burgess" },
+    { value: 85, label: "Burgess" },
+  ];
   const dataElectrothon5 = [
     { value: 1159, label: "N&J" },
     { value: 52, label: "Fraser" },
@@ -18,12 +24,18 @@ const ChartSection = () => {
     { value: 66, label: "Fraser" },
     { value: 279, label: "Burgess" },
   ];
-  const customColors = ["#009548", "#32CD32", "#9ACD32"];
+  const customColors = ["#007D33", "#7FFF00", "#32CD32",  "#009548"];
 
   const legendPayload = [
     { value: "Total Applications", type: "disc", color: "#009548" },
     { value: "Total Projects", type: "disc", color: "#32CD32" },
-    { value: "Total Teams", type: "disc", color: "#9ACD32 " },
+    { value: "Total Teams", type: "disc", color: "#007D33" },
+  ];
+  const legendPayloadElectrothon6 = [
+    { value: "Total Applications", type: "disc", color: "#009548" },
+    { value: "Total Hardware Projects", type: "disc", color: "#7FFF00" },
+    { value: "Total Software Projects", type: "disc", color: "#32CD32" },
+    { value: "Total Teams", type: "disc", color: "#007D33" },
   ];
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -54,10 +66,10 @@ const ChartSection = () => {
   };
 
   return (
-    <div className="chart-container">
+    <div className="chart-container justify-center items-center flex flex-col space-y-16">
       <div className="chart-section">
-        <div className="tit">
-          <div className="chart-title">ELECTROTHON 5.0</div>
+        <div className="tit flex items-center">
+          <div className="chart-title">ELECTROTHON 6.0</div>
           <div className="online">offline</div>
         </div>
 
@@ -65,151 +77,197 @@ const ChartSection = () => {
           <PieChart>
             <Pie
               dataKey="value"
-              data={dataElectrothon5}
+              data={dataElectrothon6}
               cx="50%"
               cy="50%"
               outerRadius={
                 windowWidth <= 650
                   ? "100%"
                   : windowWidth <= 883
-                  ? "50%"
-                  : windowWidth <= 900
-                  ? "60%"
-                  : windowWidth <= 1300
-                  ? "60%"
-                  : "75%"
+                    ? "50%"
+                    : windowWidth <= 900
+                      ? "60%"
+                      : windowWidth <= 1300
+                        ? "60%"
+                        : "75%"
               }
               label
             >
-              {dataElectrothon5.map((entry, index) => (
+              {dataElectrothon6.map((entry, index) => (
                 <Cell
                   key={`cell-${index}`}
                   fill={customColors[index % customColors.length]}
                 />
               ))}
             </Pie>
-            {windowWidth <= 650 && (
+            {/* {windowWidth <= 650 && ( */}
               <Legend
-                payload={legendPayload}
+                payload={legendPayloadElectrothon6}
                 align="center"
                 verticalAlign="bottom"
               />
-            )}
+            {/* )} */}
           </PieChart>
         </ResponsiveContainer>
-      </div>
 
-      <div className="chart-section">
-        <div className="tit">
-          <div className="chart-title">ELECTROTHON 4.0</div>
-          <div className="online">online</div>
+      </div>
+      <div className="chart-container">
+        <div className="chart-section">
+          <div className="tit">
+            <div className="chart-title">ELECTROTHON 5.0</div>
+            <div className="online">offline</div>
+          </div>
+
+          <ResponsiveContainer width="100%" height={calculateHeight()}>
+            <PieChart>
+              <Pie
+                dataKey="value"
+                data={dataElectrothon5}
+                cx="50%"
+                cy="50%"
+                outerRadius={
+                  windowWidth <= 650
+                    ? "100%"
+                    : windowWidth <= 883
+                      ? "50%"
+                      : windowWidth <= 900
+                        ? "60%"
+                        : windowWidth <= 1300
+                          ? "60%"
+                          : "75%"
+                }
+                label
+              >
+                {dataElectrothon5.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={customColors[index % customColors.length]}
+                  />
+                ))}
+              </Pie>
+              {windowWidth <= 650 && (
+                <Legend
+                  payload={legendPayload}
+                  align="center"
+                  verticalAlign="bottom"
+                />
+              )}
+            </PieChart>
+          </ResponsiveContainer>
         </div>
 
-        <ResponsiveContainer width="100%" height={calculateHeight()}>
-          <PieChart>
-            <Pie
-              dataKey="value"
-              data={dataElectrothon4}
-              cx="50%"
-              cy="50%"
-              outerRadius={
-                windowWidth <= 650
-                  ? "100%"
-                  : windowWidth <= 883
-                  ? "50%"
-                  : windowWidth <= 900
-                  ? "60%"
-                  : windowWidth <= 1300
-                  ? "60%"
-                  : "75%"
-              }
-              label
-            >
-              {dataElectrothon4.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={customColors[index % customColors.length]}
+        <div className="chart-section">
+          <div className="tit">
+            <div className="chart-title">ELECTROTHON 4.0</div>
+            <div className="online">online</div>
+          </div>
+
+          <ResponsiveContainer width="100%" height={calculateHeight()}>
+            <PieChart>
+              <Pie
+                dataKey="value"
+                data={dataElectrothon4}
+                cx="50%"
+                cy="50%"
+                outerRadius={
+                  windowWidth <= 650
+                    ? "100%"
+                    : windowWidth <= 883
+                      ? "50%"
+                      : windowWidth <= 900
+                        ? "60%"
+                        : windowWidth <= 1300
+                          ? "60%"
+                          : "75%"
+                }
+                label
+              >
+                {dataElectrothon4.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={customColors[index % customColors.length]}
+                  />
+                ))}
+              </Pie>
+              {windowWidth <= 650 && (
+                <Legend
+                  payload={legendPayload}
+                  align="center"
+                  verticalAlign="bottom"
                 />
-              ))}
-            </Pie>
-            {windowWidth <= 650 && (
-              <Legend
-                payload={legendPayload}
-                align="center"
-                verticalAlign="bottom"
-              />
-            )}
-          </PieChart>
-        </ResponsiveContainer>
+              )}
+            </PieChart>
+          </ResponsiveContainer>
 
-        {windowWidth > 1000 && (
-          <Legend
-            payload={legendPayload}
-            align="center"
-            verticalAlign="bottom"
-            wrapperStyle={{ position: "absolute", bottom: "-5%", left: "35%" }}
-          />
-        )}
-        {windowWidth < 1000 && windowWidth > 850 && (
-          <Legend
-            payload={legendPayload}
-            align="center"
-            verticalAlign="bottom"
-            wrapperStyle={{ position: "absolute", bottom: "-5%", left: "25%" }}
-          />
-        )}
-        {windowWidth <= 850 && windowWidth > 650 && (
-          <Legend
-            payload={legendPayload}
-            align="center"
-            verticalAlign="bottom"
-            wrapperStyle={{ position: "absolute", bottom: "-5%", left: "20%" }}
-          />
-        )}
-      </div>
-
-      <div className="chart-section">
-        <div className="tit">
-          <div className="chart-title">ELECTROTHON 3.0</div>
-          <div className="online">online</div>
+          {windowWidth > 1000 && (
+            <Legend
+              payload={legendPayload}
+              align="center"
+              verticalAlign="bottom"
+              wrapperStyle={{ position: "absolute", bottom: "-5%", left: "35%" }}
+            />
+          )}
+          {windowWidth < 1000 && windowWidth > 850 && (
+            <Legend
+              payload={legendPayload}
+              align="center"
+              verticalAlign="bottom"
+              wrapperStyle={{ position: "absolute", bottom: "-5%", left: "25%" }}
+            />
+          )}
+          {windowWidth <= 850 && windowWidth > 650 && (
+            <Legend
+              payload={legendPayload}
+              align="center"
+              verticalAlign="bottom"
+              wrapperStyle={{ position: "absolute", bottom: "-5%", left: "20%" }}
+            />
+          )}
         </div>
 
-        <ResponsiveContainer width="100%" height={calculateHeight()}>
-          <PieChart style={{ marginBottom: 8 }}>
-            <Pie
-              dataKey="value"
-              data={dataElectrothon3}
-              cx="50%"
-              cy="50%"
-              outerRadius={
-                windowWidth <= 650
-                  ? "100%"
-                  : windowWidth <= 883
-                  ? "50%"
-                  : windowWidth <= 900
-                  ? "60%"
-                  : windowWidth <= 1300
-                  ? "60%"
-                  : "75%"
-              }
-              label
-            >
-              {dataElectrothon3.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={customColors[index % customColors.length]}
+        <div className="chart-section">
+          <div className="tit">
+            <div className="chart-title">ELECTROTHON 3.0</div>
+            <div className="online">online</div>
+          </div>
+
+          <ResponsiveContainer width="100%" height={calculateHeight()}>
+            <PieChart style={{ marginBottom: 8 }}>
+              <Pie
+                dataKey="value"
+                data={dataElectrothon3}
+                cx="50%"
+                cy="50%"
+                outerRadius={
+                  windowWidth <= 650
+                    ? "100%"
+                    : windowWidth <= 883
+                      ? "50%"
+                      : windowWidth <= 900
+                        ? "60%"
+                        : windowWidth <= 1300
+                          ? "60%"
+                          : "75%"
+                }
+                label
+              >
+                {dataElectrothon3.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={customColors[index % customColors.length]}
+                  />
+                ))}
+              </Pie>
+              {windowWidth <= 650 && (
+                <Legend
+                  payload={legendPayload}
+                  align="center"
+                  verticalAlign="bottom"
                 />
-              ))}
-            </Pie>
-            {windowWidth <= 650 && (
-              <Legend
-                payload={legendPayload}
-                align="center"
-                verticalAlign="bottom"
-              />
-            )}
-          </PieChart>
-        </ResponsiveContainer>
+              )}
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
